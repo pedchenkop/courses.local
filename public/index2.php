a8344c498430463d03147029cfbf1bf0a69611ed
Artem Pedchenko <sale@pampam.com.ua>
2014-05-18 10:30:17 +0200

    first commit

---
+++ public/index2.php
@@ -0,0 +1,15 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
+
+$logger = new Logger('my_logger');
+$logger->pushHandler(new StreamHandler(__DIR__.'/log/my_app.log', Logger::DEBUG));
+$logger->pushHandler(new FirePHPHandler());
+
+var_dump(__DIR__);
+
+$logger->addInfo('My Logger is now ready!');
